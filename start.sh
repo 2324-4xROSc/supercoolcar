4cffdb594d333e7962e1ef74baefa2f8e1ad97dc
xalivo <87896066+xalivo@users.noreply.github.com>
2024-12-10 11:46:41 +0100

    sync led-indicator, servo, websocket & create requirements.txt

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 sudo python3 ./imu/imu.py &
-python3 camera/camera_publish_websocket.py &
-python3 gps/gps\ nmea\ parser.py &
-sudo python3 tof/TimeOfFlightSensor.py &
-sudo python3 voltage-sensor/VoltageSensor.py &
-python3 websocket/mqtt_subscriber.py
-
+python3 ./camera/camera_publish_websocket.py &
+python3 ./gps/gps\ nmea\ parser.py &
+sudo python3 ./tof/TimeOfFlightSensor.py &
+sudo python3 ./voltage-sensor/VoltageSensor.py &
+python3 ./websocket/mqtt_subscriber.py &
+sudo python3 ./servo/drive_steer.py
