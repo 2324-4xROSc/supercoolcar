a18881e5556afc8ec2674e5c17607432e615a2c4
xalivo <87896066+xalivo@users.noreply.github.com>
2024-12-10 11:11:05 +0100

    created start script

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+sudo python3 ./imu/imu.py &
+python3 camera/camera_publish_websocket.py &
+python3 gps/gps\ nmea\ parser.py &
+sudo python3 tof/TimeOfFlightSensor.py &
+sudo python3 voltage-sensor/VoltageSensor.py &
+python3 websocket/mqtt_subscriber.py
+
